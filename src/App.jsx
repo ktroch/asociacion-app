0cc88d085db61baa83011ef793adb1d178f5b27a
ktroch <ktroch.opt.fp@gmail.com>
2026-05-09 20:46:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">¡Tailwind funciona!</h1>
+    </div>
+  )
+}
